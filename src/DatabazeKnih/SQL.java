0b6b046e681f7bf9a6cf4f92cf11bdac22bd3d8d
vitabulva <bessca87@gmail.com>
2024-05-05 22:38:16 +0200

    Sixth commit

--- src/DatabazeKnih/SQL.java
+++ src/DatabazeKnih/SQL.java
@@ -7,7 +7,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Set;
-/*
+import java.util.TreeSet;
+
+import static DatabazeKnih.Databaze.knihy;
+import static DatabazeKnih.Romany.Zanr.*;
+
 public class SQL {
     private static final SQL INSTANCE = new SQL();
     private SQL() {}
@@ -15,7 +19,7 @@ public class SQL {
         return INSTANCE;
     }
 
-    private Connection conn;
+    public Connection conn;
     private int numberOfBook = 0;
 
     public boolean connect() {
@@ -37,31 +41,29 @@ public class SQL {
             catch (SQLException ex) {System.out.println(ex.getMessage());}
         }
     }
-    public boolean createTables(){
+    public boolean novaTabulka(){
         if (conn==null) return false;
+        try{
         String sql_k = "CREATE TABLE IF NOT EXISTS knihy ("
                 + "id integer PRIMARY KEY,"
                 + "jmeno varchar NOT NULL,"
                 + "rocnik integer NOT NULL,"
                 + "dostupnost bit NOT NULL,"
                 + "typ integer,"
-                + "property integer" + ");";
-       /* String sql_a = "CREATE TABLE IF NOT EXISTS autori ("
-                + "id integer PRIMARY KEY,"
-                + "knihaid integer FOREGIN KEY,"
-                + "jmeno varchar NOT NULL" + ");";*/
-        /*try{
+                + "property integer,"
+                + "knihaid integer FOREIGN KEY )";
+
             Statement stmt = conn.createStatement();
             stmt.executeUpdate(sql_k);
             stmt.close();
             return true;
         }
-        catch (SQLException e) {
+        catch(SQLException e){
             System.out.println(e.getMessage());
+            return false;
         }
-        return false;
     }
-    public boolean dropTables() {
+    public boolean vyprazdniTabulku() {
         if (conn==null) return false;
         String sql_k = "DROP TABLE knihy";
         try{
@@ -78,23 +80,17 @@ public class SQL {
         if (conn==null) return false;
         try {
             numberOfBook++;
-            String sql = "INSERT INTO knihy(id,jmeno,rocnik,dostupnost,typ,property) VALUES(?,?,?,?,?,?)";
+            String sql = "INSERT INTO knihy(id,jmeno,autor,rocnik,dostupnost,typ,property) VALUES(?,?,?,?,?,?,?)";
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setInt(1, numberOfBook);
             pstmt.setString(2, kniha.getJmeno());
-            pstmt.setInt(3, kniha.getRokVydani());
-            pstmt.setBoolean(4, kniha.dostupnost);
-            pstmt.setInt(5, (kniha instanceof Romany)?1:2);
-            if(kniha instanceof Romany) {pstmt.setInt(6, ((Romany)kniha).getZanr().ordinal());}
-            else {pstmt.setInt(6, ((Ucebnice)kniha).getRocnik());}
+            pstmt.setString(3, kniha.getAutor());
+            pstmt.setInt(4, kniha.getRokVydani());
+            pstmt.setBoolean(5, kniha.dostupnost);
+            pstmt.setInt(6, (kniha instanceof Romany)?1:2);
+            if(kniha instanceof Romany) {pstmt.setInt(7, ((Romany)kniha).getZanr().ordinal());}
+            else {pstmt.setInt(7, ((Ucebnice)kniha).getRocnik());}
             pstmt.executeUpdate();
-            for(String autor:kniha.getAutor()) {
-                sql = "INSERT INTO autori(knihaid,jmeno) VALUES(?,?)";
-                pstmt = conn.prepareStatement(sql);
-                pstmt.setInt(1, numberOfBook);
-                pstmt.setString(2, autor);
-                pstmt.executeUpdate();
-            }
             pstmt.close();
             return true;
         } catch (SQLException e) {
@@ -102,44 +98,56 @@ public class SQL {
         }
         return false;
     }
-    public Set<Kniha> knihy databasePull() {
-        Set<Kniha> knihy;
+    public Set<Kniha> knihyDatabasePull() {
+
+        boolean run = true;
+        Romany.Zanr hledanyZanr = null;
+
+        Set<Kniha> knihy = new TreeSet<>();
+
         if (conn==null) return knihy;
         try {
             String sql_k = "SELECT * FROM knihy";
             Statement stmt  = conn.createStatement();
             ResultSet rs_k  = stmt.executeQuery(sql_k);
-            List<String> autori = new ArrayList<>();
             while (rs_k.next()) {
-                String sql_a = "SELECT jmeno FROM autori WHERE knihaid = ?";
+                Set<String> autorSet = new TreeSet<>();
+                String sql_a = "SELECT jmeno FROM autor WHERE knihaid = ?";
                 PreparedStatement pstmt = conn.prepareStatement(sql_a);
                 pstmt.setInt(1, rs_k.getInt("id"));
-                ResultSet rs_a  = pstmt.executeQuery();
-                while(rs_a.next()) {autori.add(rs_a.getString("jmeno"));}
+                ResultSet rs_a = pstmt.executeQuery();
+                while (rs_a.next()) {
+                    autorSet.add(rs_a.getString("jmeno"));
+                }
+                Romany.Zanr zanr = null;
                 switch(rs_k.getInt("typ")) {
                     case 1:
-                        Zanr zanr;
                         switch(rs_k.getInt("property")) {
                             case 1:
-                                zanr = Zanr.AUTOBIOGRAFICKY;
+                                hledanyZanr = Komiksy;
+                                run = false;
                                 break;
                             case 2:
-                                zanr = Zanr.DETEKTIVNI;
+                                hledanyZanr = Kucharky;
+                                run = false;
                                 break;
                             case 3:
-                                zanr = Zanr.KLICOVY;
+                                hledanyZanr = Pohadky;
+                                run = false;
                                 break;
                             case 4:
-                                zanr = Zanr.SCIFI;
+                                hledanyZanr = Detektivky;
+                                run = false;
                                 break;
-                            default:
-                                zanr = Zanr.UTOPICKY;
+                            case 5:
+                                hledanyZanr = Horory;
+                                run = false;
                                 break;
                         }
-                        knihy.add(new Romany(rs_k.getString("jmeno"),autor,rs_k.getInt("rocnik"),zanr,rs_k.getBoolean("dostupnost")));
+                        knihy.add(new Romany(rs_k.getString("jmeno"),rs_k.getString("autor"),rs_k.getInt("rokVydani"), rs_k.getBoolean("dostupnost"), zanr));
                         break;
                     case 2:
-                        knihy.add(new Ucebnice(rs_k.getString("jmeno"),autor,rs_k.getInt("rocnik"),rs_k.getInt("property"),rs_k.getBoolean("dostupnost")));
+                        knihy.add(new Ucebnice(rs_k.getString("jmeno"),rs_k.getString("autor"),rs_k.getInt("rokVydani"), rs_k.getBoolean("dostupnost"), rs_k.getInt ("rocnik")));
                 }
             }
         } catch (SQLException e) {
@@ -148,4 +156,4 @@ public class SQL {
         return knihy;
     }
 
-}*/
+}
